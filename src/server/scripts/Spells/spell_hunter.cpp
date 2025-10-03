68fa3f96c106a90d75a9cf212beee41265fa5602
thomasjteachey <thomasjteachey@gmail.com>
2025-10-03 19:33:34 -0400

    some stuff

--- src/server/scripts/Spells/spell_hunter.cpp
+++ src/server/scripts/Spells/spell_hunter.cpp
@@ -1710,6 +1710,28 @@ class spell_hun_trap_cd_reduce : public SpellScript
     }
 };
 
+// 81386 - Intimidate CD Reduce
+class spell_int_cd_reduce : public SpellScript
+{
+    PrepareSpellScript(spell_int_cd_reduce);
+
+    bool Load() override
+    {
+        return GetCaster()->GetTypeId() == TYPEID_PLAYER;
+    }
+
+    void HandleDummy(SpellEffIndex /*effIndex*/)
+    {
+        uint32 cdreduce = GetEffectValue();
+        GetCaster()->GetSpellHistory()->ModifyCooldown(19577, cdreduce);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_int_cd_reduce::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+    }
+};
+
 void AddSC_hunter_spell_scripts()
 {
     RegisterSpellScript(spell_hun_aspect_of_the_beast);
@@ -1755,4 +1777,5 @@ void AddSC_hunter_spell_scripts()
     RegisterSpellScript(spell_hun_outmaneuver);
     RegisterSpellScript(spell_hun_weaving);
     RegisterSpellScript(spell_hun_trap_cd_reduce);
+    RegisterSpellScript(spell_int_cd_reduce);
 }
