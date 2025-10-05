7c2b87da1d756609ef258fe107175c1b2d735888
thomasjteachey <thomasjteachey@gmail.com>
2025-10-05 00:13:19 -0400

    Revert "Prevent pets from using steep ground paths"
    This reverts commit 6f1b90986e8dc2e2864d5cf604035561d23efe5e.

--- src/server/game/Movement/PathGenerator.cpp
+++ src/server/game/Movement/PathGenerator.cpp
@@ -688,11 +688,8 @@ void PathGenerator::UpdateFilter()
         }
 
         if (Creature const* _sourceCreature = _source->ToCreature())
-        {
-            if (!_sourceCreature->IsPet() && !_sourceCreature->IsControlledByPlayer())
-                if (_sourceCreature->IsInCombat() || _sourceCreature->IsInEvadeMode())
-                    _filter.setIncludeFlags(_filter.getIncludeFlags() | NAV_GROUND_STEEP);
-        }
+            if (_sourceCreature->IsInCombat() || _sourceCreature->IsInEvadeMode())
+                _filter.setIncludeFlags(_filter.getIncludeFlags() | NAV_GROUND_STEEP);
     }
 }
 
