a6ca1c79efeb076edaed1f904dcdc35e3901f25f
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-10-22 20:45:20 -0400

    Fix AutoBalance include path for game

---
+++ src/server/scripts/Custom/AutoBalance/AutoBalanceLoader.cpp
@@ -0,0 +1,20 @@
+#include "Custom/AutoBalance/AutoBalance.h"
+
+namespace
+{
+    class DefaultAutoBalanceModule final : public ABModuleScript
+    {
+    public:
+        DefaultAutoBalanceModule()
+            : ABModuleScript("DefaultAutoBalanceModule")
+        {
+        }
+
+        void OnMessage(AutoBalance::Message const& /*message*/) override { }
+    };
+}
+
+void AddAutoBalanceScripts()
+{
+    new DefaultAutoBalanceModule();
+}
