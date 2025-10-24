f1b5e9acdc3b01b9a228b6fc12a113099f2a2757
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-10-24 08:18:03 -0400

    Fix AutoBalance stat updates for 1.12 APIs

--- src/server/scripts/Custom/AutoBalance/AutoBalanceLoader.cpp
+++ src/server/scripts/Custom/AutoBalance/AutoBalanceLoader.cpp
@@ -1,4 +1,6 @@
 #include "AutoBalance.h"
+#include "AutoBalance/AutoBalanceCreature.h"
+#include "ScriptMgr.h"
 
 namespace
 {
@@ -12,9 +14,24 @@ namespace
 
         void OnMessage(AutoBalance::Message const& /*message*/) override { }
     };
+
+    class AutoBalanceCreatureUpdateScript final : public AllCreatureScript
+    {
+    public:
+        AutoBalanceCreatureUpdateScript()
+            : AllCreatureScript("AutoBalanceCreatureUpdateScript")
+        {
+        }
+
+        void OnAllCreatureUpdate(Creature* creature, uint32 /*diff*/) override
+        {
+            AutoBalance::ScaleCreature(creature);
+        }
+    };
 }
 
 void AddAutoBalanceScripts()
 {
     new DefaultAutoBalanceModule();
+    new AutoBalanceCreatureUpdateScript();
 }
