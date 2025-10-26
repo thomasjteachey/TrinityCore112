70f7373c75c302a4d2dc27d74de06aaefe87b7c1
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-10-26 18:55:33 -0400

    Fix AutoBalance config loader lambda

--- src/server/game/AutoBalance/AutoBalanceCreature.h
+++ src/server/game/AutoBalance/AutoBalanceCreature.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include "AutoBalance/AutoBalanceConfig.h"
 #include "AutoBalance/AutoBalanceCreatureInfo.h"
 #include "Optional.h"
 
 class Creature;
+class Map;
 class Unit;
 
 namespace AutoBalance
 {
+    struct ActiveInflectionInfo
+    {
+        InflectionPointSettings Settings;
+        uint32 TargetPlayers = 1;
+    };
+
     AutoBalanceCreatureInfo& GetCreatureInfo(Creature& creature);
     AutoBalanceCreatureInfo const& GetCreatureInfo(Creature const& creature);
 
@@ -16,6 +24,8 @@ namespace AutoBalance
 
     Optional<float> GetDamageHealingMultiplier(Unit const* unit);
     Optional<float> GetCrowdControlDurationMultiplier(Unit const* unit);
+
+    ActiveInflectionInfo GetInflectionInfoForMap(Map const* map, bool forBoss);
 
     void ScaleCreature(Creature* creature);
 }
