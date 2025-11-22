2484747a85d658eebc498e3aa6c33e71c2fe85bf
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-11-22 00:52:26 -0500

    Use struct for player run lockouts

--- src/server/scripts/Custom/mod_pvpve_dungeon.h
+++ src/server/scripts/Custom/mod_pvpve_dungeon.h
@@ -164,8 +164,9 @@ private:
     using QueueContainer = std::map<uint64, QueuedTeam>;
     using RunContainer = std::map<uint64, PvpveDungeonRun>;
     using TeamContainer = std::map<uint64, PvpveTeam>;
-    using PlayerRunMap = std::map<ObjectGuid, PlayerRunLockout>;
+    using PlayerRunMap = std::map<ObjectGuid, uint64>;
     using PlayerTeamMap = std::map<ObjectGuid, uint64>;
+    using PlayerLockoutMap = std::map<ObjectGuid, PlayerRunLockout>;
     using TemplateContainer = std::map<uint32, DungeonTemplate>;
     using SpawnContainer = std::map<uint32, std::vector<SpawnPoint>>;
     using PlayerLocationMap = std::map<ObjectGuid, WorldLocation>;
@@ -175,7 +176,7 @@ private:
     TeamContainer _teams;
     PlayerRunMap _playerToRun;
     PlayerTeamMap _playerToTeam;
-    PlayerRunMap _playerRunLockouts;
+    PlayerLockoutMap _playerRunLockouts;
     TemplateContainer _templates;
     SpawnContainer _spawns;
     GuidSet _queuedPlayers;
