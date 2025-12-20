107901e885b4e89f40e85df9489f94629041189f
thomasjteachey <thomasjteachey@gmail.com>
2025-12-20 01:55:47 -0500

    further rehgar fury nerf

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2232,7 +2232,7 @@ void Unit::CompleteGhostWolfCharge(Unit* target)
 
     if (SpellHistory* spellHistory = GetSpellHistory())
     {
-        static constexpr std::chrono::seconds GhostWolfCooldown(15);
+        static constexpr std::chrono::seconds GhostWolfCooldown(20);
         SpellInfo const* ghostWolfInfo = sSpellMgr->GetSpellInfo(SPELL_SHAMAN_GHOST_WOLF);
 
         if (ghostWolfInfo)
