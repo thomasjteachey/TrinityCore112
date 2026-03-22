19f334fe770486cc9b93fbd2471d264e711a691a
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-03-22 07:57:35 -0400

    WSG: remove auto-return respawn sound for dropped flags

--- src/server/game/Battlegrounds/Zones/BattlegroundWS.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundWS.cpp
@@ -265,7 +265,6 @@ void BattlegroundWS::RespawnFlagAfterDrop(uint32 team)
         SpawnBGObject(BG_WS_OBJECT_H_FLAG, RESPAWN_IMMEDIATELY);
 
     SendBroadcastText(team == ALLIANCE ? BG_WS_TEXT_ALLIANCE_FLAG_RETURNED : BG_WS_TEXT_HORDE_FLAG_RETURNED, CHAT_MSG_BG_SYSTEM_NEUTRAL);
-    PlaySoundToAll(BG_WS_SOUND_FLAGS_RESPAWNED);
 
     if (GameObject* obj = GetBgMap()->GetGameObject(GetDroppedFlagGUID(team)))
         obj->Delete();
