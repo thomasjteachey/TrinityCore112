fa64f02c3e46ed717129c98f6334d87f6174f548
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-04-08 17:50:43 -0400

    Fix moving playerbots stopping when turning to instant cast

--- playerbot reference/mod-playerbots-master/src/Util/ServerFacade.cpp
+++ playerbot reference/mod-playerbots-master/src/Util/ServerFacade.cpp
@@ -46,15 +46,16 @@ void ServerFacade::SetFacingTo(Player* bot, WorldObject* wo, bool force)
 
     float angle = bot->GetAngle(wo);
 
-    // if (!force && bot->isMoving())
-    //     bot->SetFacingTo(bot->GetAngle(wo));
-    // else
-    // {
+    if (!force && bot->isMoving())
+    {
+        bot->SetFacingTo(angle);
+        return;
+    }
+
     bot->SetOrientation(angle);
 
     if (!bot->IsRooted())
         bot->SendMovementFlagUpdate();
-    // }
 }
 
 Unit* ServerFacade::GetChaseTarget(Unit* target)
