b9f9f261f0568415b19e5ea093761289419f5c74
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-04-11 04:38:50 -0400

    Adjust Rehgar's Fury Ghost Wolf cooldown to 15s

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2254,7 +2254,7 @@ void Unit::CompleteGhostWolfCharge(Unit* target)
 
     if (SpellHistory* spellHistory = GetSpellHistory())
     {
-        static constexpr std::chrono::seconds GhostWolfCooldown(18);
+        static constexpr std::chrono::seconds GhostWolfCooldown(15);
         SpellInfo const* ghostWolfInfo = sSpellMgr->GetSpellInfo(SPELL_SHAMAN_GHOST_WOLF);
 
         if (ghostWolfInfo)
