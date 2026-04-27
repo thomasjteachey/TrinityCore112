4fcbdfc30c65c74f7532e3ee4efb09a515183e22
thomasjteachey <thomasjteachey@gmail.com>
2026-04-27 07:31:48 -0400

    hmmm

--- src/server/game/Battlegrounds/Zones/BattlegroundSCM.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundSCM.cpp
@@ -101,8 +101,6 @@ uint32 BattlegroundSCM::GetHonorRewardForTeam() const
 
     if (!_humanFaceoffEverHappened)
         reward /= 2;
-
-    return (reward * 3) / 5;
 }
 
 void BattlegroundSCM::ModifyEndOfMatchHonorRewards(uint32 winner, uint32 team, uint32& winnerHonor, uint32& /*loserHonor*/) const
@@ -116,7 +114,7 @@ void BattlegroundSCM::ModifyEndOfMatchHonorRewards(uint32 winner, uint32 team, u
     if (!_humanFaceoffEverHappened)
         winnerHonor /= 2;
 
-    winnerHonor = (winnerHonor * 3) / 5;
+    winnerHonor = (winnerHonor * 3.5) / 5;
 }
 
 bool BattlegroundSCM::SetupBattleground()
