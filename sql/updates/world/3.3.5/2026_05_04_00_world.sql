2c4e014db46e744d89849772ea0ad1763e6a692d
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-05-04 16:39:54 -0400

    scripts/warrior: trigger 89753 on stance cast with aura 89751

---
+++ sql/updates/world/3.3.5/2026_05_04_00_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (71, 2457, 2458) AND `ScriptName`='spell_warr_stance_switch_trigger';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(71, 'spell_warr_stance_switch_trigger'),
+(2457, 'spell_warr_stance_switch_trigger'),
+(2458, 'spell_warr_stance_switch_trigger');
