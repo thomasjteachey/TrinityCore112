ec365b93bc37d1debfff886517b0caa7dc7e0e31
Thomas Teachey <tteachey@installationmadeeasy.com>
2026-05-10 00:34:43 -0400

    god dammit

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -6576,7 +6576,7 @@ SpellCastResult Spell::CheckCasterAuras(uint32* param1) const
         result = SPELL_FAILED_CHARMED;
 
     bool hasAttackMeFear = unitCaster->HasAttackMeFearAura();
-    if (result == SPELL_CAST_OK && hasAttackMeFear && !CheckSpellCancelsCharm(param1))
+    if (result == SPELL_CAST_OK && hasAttackMeFear && !CheckSpellCancelsTaunt(param1))
         result = SPELL_FAILED_CHARMED;
 
     // spell has attribute usable while having a cc state, check if caster has allowed mechanic auras, another mechanic types must prevent cast spell
@@ -6650,7 +6650,7 @@ SpellCastResult Spell::CheckCasterAuras(uint32* param1) const
             result = SPELL_FAILED_STUNNED;
     }
 
-    if (result == SPELL_CAST_OK && unitCaster->IsTaunted() && !CheckSpellCancelsCharm(param1)) //wut
+    if (result == SPELL_CAST_OK && unitCaster->IsTaunted() && !CheckSpellCancelsTaunt(param1)) //wut
         result = SPELL_FAILED_CHARMED;
     if (result == SPELL_CAST_OK && unitflag & UNIT_FLAG_SILENCED && m_spellInfo->PreventionType == SPELL_PREVENTION_TYPE_SILENCE && !CheckSpellCancelsSilence(param1))
         result = SPELL_FAILED_SILENCED;
