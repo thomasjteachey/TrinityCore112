c5aaf15f42126d13282e4b3a4bc6cf4c7fee1d7e
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-05-23 08:29:55 -0400

    Add custom item flag to bypass vendor class visibility filter

--- src/server/game/Entities/Item/ItemTemplate.h
+++ src/server/game/Entities/Item/ItemTemplate.h
@@ -223,7 +223,8 @@ enum ItemFlagsCustom
 {
     ITEM_FLAGS_CU_DURATION_REAL_TIME    = 0x0001,   // Item duration will tick even if player is offline
     ITEM_FLAGS_CU_IGNORE_QUEST_STATUS   = 0x0002,   // No quest status will be checked when this item drops
-    ITEM_FLAGS_CU_FOLLOW_LOOT_RULES     = 0x0004    // Item will always follow group/master/need before greed looting rules
+    ITEM_FLAGS_CU_FOLLOW_LOOT_RULES     = 0x0004,   // Item will always follow group/master/need before greed looting rules
+    ITEM_FLAGS_CU_VENDOR_SHOW_ALL_CLASSES = 0x0008  // Item is always shown in vendor inventory regardless of buyer class restriction
 };
 
 enum BAG_FAMILY_MASK
