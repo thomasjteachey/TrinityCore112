119961e56a7e17b095dff3aca6fefdc9756933fd
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2026-06-16 13:35:16 -0400

    Expand Sap hit diagnostics

---
+++ sql/updates/world/3.3.5/2026_06_16_00_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (6770, 2070, 11297) AND `ScriptName`='spell_rog_sap_diagnostic';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(6770, 'spell_rog_sap_diagnostic'),
+(2070, 'spell_rog_sap_diagnostic'),
+(11297, 'spell_rog_sap_diagnostic');
