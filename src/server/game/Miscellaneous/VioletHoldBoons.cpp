372a1602d2622b28a8feab592694795431f43e76
thomasjteachey <thomasjteachey@gmail.com>
2026-08-16 00:12:49 -0400

    verbiage change

--- src/server/game/Miscellaneous/VioletHoldBoons.cpp
+++ src/server/game/Miscellaneous/VioletHoldBoons.cpp
@@ -69,7 +69,7 @@ BoonInfo const kBoons[uint8(Boon::Max)] =
     { Boon::SpellCrit,    SPELL_BOON_SPELL_CRIT,   3, 255, MASK_CASTER,   5, "Boon of Insight",      "spell crit",                              "%" },
     { Boon::AttackSpeed,  SPELL_BOON_ATTACK_SPEED, 4, 255, MASK_MELEE,    6, "Boon of Haste",        "attack speed",                            "%" },
     { Boon::CastSpeed,    SPELL_BOON_CAST_SPEED,   4, 100, MASK_CASTER,   6, "Boon of Celerity",     "casting speed",                           "%" },
-    { Boon::MountSpeed,   SPELL_BOON_MOUNT_SPEED,  1,   1, MASK_ALL,     2, "Boon of the Outrider", "mounts summon 3 sec faster, even in combat (unique)",     "" },
+    { Boon::MountSpeed,   SPELL_BOON_MOUNT_SPEED,  1,   1, MASK_ALL,     2, "Boon of the Outrider", "mounts summon 3 sec faster (unique)",     "" },
     { Boon::Cooldown,     SPELL_BOON_COOLDOWN,     5, 100, MASK_ALL,     4, "Boon of Alacrity",     "cooldown reduction",                      "%" },
     { Boon::Resistance,   SPELL_BOON_RESISTANCE,   5, 255, MASK_ALL,      6, "Boon of Warding",      "to all resistances",                      "" },
     { Boon::Level,        SPELL_BOON_LEVEL,        1, 100, MASK_ALL,     3, "Boon of Ascension",    "level, with its talent point (until you leave)", "" },
